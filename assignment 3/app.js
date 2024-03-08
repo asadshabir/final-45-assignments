8b4a0519dcd1ca6321ca509c6bee7e0fc059dcdf
asadshabir <asadshabir505@gmail.com>
2024-03-08 16:30:22 +0500

    first commit

---
+++ assignment 3/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous quote an its authore
+let quote = "Nothihng is impossible because impopsible it self i am possible";
+let authore = "Asad shabir";
+// printing the message 
+console.log(`"${quote}" - ${authore}`);
---
+++ assignment 3/app.ts
@@ -0,0 +1,9 @@
+ // storing the famous quote an its authore
+ 
+ let quote:string ="Nothihng is impossible because impopsible it self i am possible"
+
+ let authore:string = "Asad shabir"
+
+ // printing the message 
+
+ console.log(`"${quote}" - ${authore}`)
